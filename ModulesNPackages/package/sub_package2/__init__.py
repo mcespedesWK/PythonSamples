62a40a388aa936fbe5cf2e0651de6a66726ae310
Mauro Céspedes Araya <mauro.cespedesaraya@wolterskluwer.com>
2022-10-24 10:57:54 +0200

    ee

--- ModulesNPackages/package/sub_package2/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 17 17:50:04 2022
-
-@author: Mauro
-"""
-
---
+++ decorators/fibonacci.py
@@ -0,0 +1,9 @@
+def fib(n):
+    if n < 2:
+        return n
+    else:
+        return fib(n-1)+fib(n-2)
+
+fibnum = int(input('Enter a number'))
+
+print(fib(fibnum))
